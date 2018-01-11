f4154c725b961f8e5b9b3c6a5d7750e776f11f4e
rover <rover@iMac.local>
2018-01-11 18:27:08 +0800

    init

---
+++ dearbinge-openapi/src/main/java/com/dearbinge/openapi/App4OpenApi.java
@@ -0,0 +1,29 @@
+package com.dearbinge.openapi;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource({ "classpath:applicationContext.xml", "dubbo-services.xml" })
+@EnableAutoConfiguration
+//@ComponentScan("com.dearbinge.controller")
+public class App4OpenApi extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App4OpenApi.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		application.listeners();
+		return application.sources(applicationClass);
+	}
+
+	private static Class<App4OpenApi> applicationClass = App4OpenApi.class;
+
+}
