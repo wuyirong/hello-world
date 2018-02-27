111b875728e7dd9130497579a3a41390c0bda9f0
daoma.gao <4aeYD*395FVmeic>
2018-02-27 12:13:33 +0800

    支持跨域访问

--- tcBaseFrame-apiGateway/src/main/java/com/tentcoo/gateway/ApiGateWayApplication.java
+++ tcBaseFrame-apiGateway/src/main/java/com/tentcoo/gateway/ApiGateWayApplication.java
@@ -3,6 +3,10 @@ package com.tentcoo.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * Created by rover on 2018/2/27.
@@ -13,6 +17,25 @@ public class ApiGateWayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGateWayApplication.class, args);
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    /**
+     * 跨域过滤器
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
     }
 
 }
